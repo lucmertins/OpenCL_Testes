7d5c329bd1616fb3e22ae8b330445661f96a7339
mertins <luciano@mertins.com.br>
2018-06-13 12:19:32 -0300

    Ajustando projeto

--- src/Principal.cpp
+++ src/Principal.cpp
@@ -12,5 +12,14 @@ using namespace std;
 int main() {
     cl_uint ret_num_platforms;
     cl_int ret = clGetPlatformIDs(0, NULL, &ret_num_platforms);
-   
+    cout << "Retorno[" << ret << "] NumPlataformas[" << ret_num_platforms << "]" << endl;
+
+    cl_platform_id platform_id = NULL;
+    cl_device_id device_id = NULL;
+    cl_uint ret_num_devices;
+    ret = clGetDeviceIDs(platform_id, CL_DEVICE_TYPE_DEFAULT, 1, &device_id, &ret_num_devices);
+    cout << "Retorno[" << ret << "] NumDevices[" << ret_num_devices << "]" << endl;
+
+     cl_context context = clCreateContext( NULL, 1, &device_id, NULL, NULL, &ret);
+
 }
