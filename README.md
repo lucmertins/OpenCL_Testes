8c223b1d60fb26ee3585f95f8c19aeb2bf6bb694
mertins <luciano@mertins.com.br>
2018-06-13 10:59:10 -0300

    Ajustando projeto

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # OpenCL_Testes
 Testando OpenCL
+
+
+Fedora install
+
+
+?? levantar procedimentos realizados anteriormente
+
+dnf install ocl-icd-devel
--- src/Principal.cpp
+++ src/Principal.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 #include "Principal.h"
 
+#ifdef __APPLE__
+#include "OpenCL/opencl.h"
+#else
+#include "CL/cl.h"
+#endif
+
 using namespace std;
 
-int main() 
-{
- cout << "Iniciando!" << endl;
- return 0;
+int main() {
+    cl_uint ret_num_platforms;
+    cl_int ret = clGetPlatformIDs(0, NULL, &ret_num_platforms);
+   
 }
